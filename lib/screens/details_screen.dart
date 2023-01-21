36a3a8db1ce5782aa004580cba712f0d792dd402
Harvey Yerik <yerik05vh@gmail.com>
2023-01-20 22:34:43 -0500

    end ui

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/widgets.dart';
 
 class DetailsScreen extends StatelessWidget {
    
@@ -12,9 +13,86 @@ class DetailsScreen extends StatelessWidget {
     return  Scaffold(
       body: CustomScrollView(
         slivers: [
-          _CustomAppBar()
+          _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              _PosterAndTitle(),
+              _Overview(),
+              CastingCards(),
+            ]),
+          )
         ],
       )
+    );
+  }
+}
+
+class _Overview extends StatelessWidget {
+  const _Overview({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0,vertical: 10.0),
+      child: Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut vulputate velit. Integer eget cursus magna, sagittis suscipit magna. Suspendisse vestibulum sapien sit amet ipsum congue gravida ut a lacus. Fusce lorem orci, commodo at fringilla a, tristique id leo. Integer sodales cursus nisl nec congue. Sed semper hendrerit augue vel lacinia. Integer vehicula nunc sit amet efficitur dignissim. Nunc non quam nunc. ",
+      textAlign: TextAlign.justify,
+      style: Theme.of(context).textTheme.subtitle1,
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme= Theme.of(context).textTheme;
+
+    return Container(
+      margin: EdgeInsets.only(top: 20.0),
+      padding: EdgeInsets.symmetric(horizontal:20.0 ),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(30.0),
+            child: const FadeInImage(
+              placeholder: AssetImage("assets/no-image.jpg"),
+              image: NetworkImage("https://via.placeholder.com/300x400"),
+              height: 150,
+            ),
+          ),
+          const SizedBox(width: 20.0,),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "title.movie",
+                style: textTheme.headline5,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                "title.movie",
+                style: textTheme.subtitle1,
+                overflow: TextOverflow.ellipsis
+              ),
+              Row(
+                children: [
+                  const Icon(Icons.star_outline, size: 15,color: Colors.grey,),
+                  const SizedBox(width: 10.0,),
+                  Text("movie.voteAverage",style:textTheme.caption)
+                ],
+              )
+            ],
+          )
+
+        ],
+      ),
     );
   }
 }
---
+++ lib/widgets/casting_cards.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class CastingCards extends StatelessWidget {
+   
+  const CastingCards({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 30.0),
+      width: double.infinity,
+      height: 225.0,
+      child: ListView.builder(
+        itemCount: 20,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (BuildContext context, index) => const _CastCard()
+      
+      ),
+    );
+  }
+}
+
+class _CastCard extends StatelessWidget {
+  const _CastCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10.0),
+      width: 110.0,
+      height: 100.0,
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: const FadeInImage(
+              placeholder: AssetImage("assets/no-image.jpg"),
+              image: NetworkImage('https://via.placeholder.com/300x400'),
+              width: 140.0,
+              height: 150.0,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox( height:  5.0 ,),
+          const Text(
+            "Un actor",
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
+    );
+  }
+}
