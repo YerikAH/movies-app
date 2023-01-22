2c5c9f76b886cbb50161ef2b142c89fbbd2b5129
Harvey Yerik <yerik05vh@gmail.com>
2023-01-21 22:52:12 -0500

    done

--- lib/providers/movie_provider.dart
+++ lib/providers/movie_provider.dart
@@ -5,16 +5,21 @@ import 'package:http/http.dart' as http;
 import 'package:movies_app/models/models.dart';
 
 
+
 class MoviesProvider extends ChangeNotifier{
-  String _apiKey = "e5cd398164428a8221fd7b3a3ab14398";
-  String _baseUrl = "api.themoviedb.org";
-  String _language = "en-US";
+  final String _apiKey = "e5cd398164428a8221fd7b3a3ab14398";
+  final String _baseUrl = "api.themoviedb.org";
+  final String _language = "en-US";
 
   List<Result> onDisplayMovies = [];
+  List<Result> onPopularMovies = [];
+
   MoviesProvider(){
     print("Movie provider inicializado");
     this.getOnDisplayMovies();
+    this.getOnDisplayMovies();
   }
+
   getOnDisplayMovies() async{
     var url =
     Uri.https(_baseUrl, '3/movie/now_playing', {
@@ -22,14 +27,25 @@ class MoviesProvider extends ChangeNotifier{
       'language':_language,
       'page':'1'
     });
-
-      // Await the http get response, then decode the json-formatted response.
     final response = await http.get(url);
 
     final decodeData =  NowPlayingResponse.fromJson(jsonDecode(response.body));
-    print(decodeData.results[0].title);
     onDisplayMovies = decodeData.results;
+    notifyListeners();
+  }
+  
+  getPopularMovies() async{
+    var url =
+    Uri.https(_baseUrl, '3/movie/popular', {
+      'api_key':_apiKey,
+      'language':_language,
+      'page':'1'
+    });
+    final response = await http.get(url);
 
+    final decodeData =  PopularResponse.fromJson(jsonDecode(response.body));
+    onPopularMovies = [...onPopularMovies,...decodeData.results];
+    print(onPopularMovies[0]);
     notifyListeners();
   }
 }
