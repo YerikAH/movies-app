256513b7b13dac363c3d75fc8d707b1cd98d87d2
Harvey Yerik <yerik05vh@gmail.com>
2023-01-28 22:38:24 -0500

    opt model

--- lib/models/search_response.dart
+++ lib/models/search_response.dart
@@ -1,7 +1,4 @@
-// To parse this JSON data, do
-//
-//     final searchResponse = searchResponseFromJson(jsonString);
-
+import 'package:movies_app/models/results_response.dart';
 import 'dart:convert';
 
 SearchResponse searchResponseFromJson(String str) => SearchResponse.fromJson(json.decode(str));
@@ -34,92 +31,5 @@ class SearchResponse {
         "total_pages": totalPages,
         "total_results": totalResults,
     };
-}
-
-class Result {
-    Result({
-        required this.adult,
-        this.backdropPath,
-        required this.genreIds,
-        required this.id,
-        required this.originalLanguage,
-        required this.originalTitle,
-        required this.overview,
-        required this.popularity,
-        this.posterPath,
-        required this.releaseDate,
-        required this.title,
-        required this.video,
-        required this.voteAverage,
-        required this.voteCount,
-    });
-
-    bool adult;
-    String? backdropPath;
-    List<int> genreIds;
-    int id;
-    OriginalLanguage originalLanguage;
-    String originalTitle;
-    String overview;
-    double popularity;
-    String? posterPath;
-    String releaseDate;
-    String title;
-    bool video;
-    double voteAverage;
-    int voteCount;
-
-    factory Result.fromJson(Map<String, dynamic> json) => Result(
-        adult: json["adult"],
-        backdropPath: json["backdrop_path"],
-        genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
-        id: json["id"],
-        originalLanguage: originalLanguageValues.map[json["original_language"]]!,
-        originalTitle: json["original_title"],
-        overview: json["overview"],
-        popularity: json["popularity"]?.toDouble(),
-        posterPath: json["poster_path"],
-        releaseDate: json["release_date"],
-        title: json["title"],
-        video: json["video"],
-        voteAverage: json["vote_average"]?.toDouble(),
-        voteCount: json["vote_count"],
-    );
-
-    Map<String, dynamic> toJson() => {
-        "adult": adult,
-        "backdrop_path": backdropPath,
-        "genre_ids": List<dynamic>.from(genreIds.map((x) => x)),
-        "id": id,
-        "original_language": originalLanguageValues.reverse[originalLanguage],
-        "original_title": originalTitle,
-        "overview": overview,
-        "popularity": popularity,
-        "poster_path": posterPath,
-        "release_date": releaseDate,
-        "title": title,
-        "video": video,
-        "vote_average": voteAverage,
-        "vote_count": voteCount,
-    };
-}
-
-enum OriginalLanguage { EN, IT }
-
-final originalLanguageValues = EnumValues({
-    "en": OriginalLanguage.EN,
-    "it": OriginalLanguage.IT
-});
-
-class EnumValues<T> {
-    Map<String, T> map;
-    late Map<T, String> reverseMap;
-
-    EnumValues(this.map);
-
-    Map<T, String> get reverse {
-        reverseMap = map.map((k, v) => MapEntry(v, k));
-        return reverseMap;
-    }
 }
 
