0976571da141aa7c97a7e9dfa8f94a3464e6f5ed
Harvey Yerik <yerik05vh@gmail.com>
2023-01-28 22:21:25 -0500

    ui widget search

--- lib/search/search_delegate.dart
+++ lib/search/search_delegate.dart
@@ -1,23 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app/theme/app_theme.dart';
+
 
 class MovieSearchDelegate extends SearchDelegate{
+  
+  @override
+  String get searchFieldLabel => "Search movie";
+
   @override
   List<Widget> buildActions(BuildContext context){
-    return [ Text(" my build actions") ];
+    return [ IconButton(
+	icon: Icon(Icons.clear),
+	onPressed: () => query = '',
+      ) ];
   }
 
   @override
   Widget buildLeading(BuildContext context){
-    return Text(" my build leading ");
+    return IconButton(
+	onPressed: () => close(context, null),
+	icon: Icon(Icons.arrow_back_rounded),
+      );
   }
 
   @override
   Widget buildResults(BuildContext context){
-    return Text("my build results");
+    return Text("my build results", style: TextStyle(color:  AppTheme.textColor));
   }
 
   @override
   Widget buildSuggestions(BuildContext context){
-    return Text("buildSuggestions");
+    if(query.isEmpty){
+      return Container(
+	child: Center(
+	  child: Icon(Icons.movie_creation_outlined, size: 100.0, color: Colors.black38,),
+	),
+      );
+    }
+    return Container();
   }
 }
